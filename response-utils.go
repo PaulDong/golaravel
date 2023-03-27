62cbd69dd24c0bdca08e45379ac6e29d7b7e1e37
paul Tung <paul@calgah.com>
2023-03-27 11:27:49 -0400

    add xml response and status of http

--- response-utils.go
+++ response-utils.go
@@ -2,7 +2,11 @@ package golaravel
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"fmt"
 	"net/http"
+	"path"
+	"path/filepath"
 )
 
 func (g *Golaravel) WriteJson(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
@@ -11,7 +15,7 @@ func (g *Golaravel) WriteJson(w http.ResponseWriter, status int, data interface{
 		return err
 	}
 	if len(headers) > 0 {
-		for key, value :=  range headers[0] {
+		for key, value := range headers[0] {
 			w.Header()[key] = value
 		}
 	}
@@ -23,4 +27,52 @@ func (g *Golaravel) WriteJson(w http.ResponseWriter, status int, data interface{
 		return err
 	}
 	return nil
+}
+
+func (g *Golaravel) WriteXML(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *Golaravel) DownloadFile(w http.ResponseWriter, r *http.Request, pathToFile, fileName string) error {
+	fp := path.Join(pathToFile, fileName)
+	fileToServe := filepath.Clean(fp)
+	w.Header().Set("Content-Type", fmt.Sprintf("attachment; file=\"%s\"", fileName))
+	http.ServeFile(w, r, fileToServe)
+	return nil
+}
+
+func (g *Golaravel) Error404(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusNotFound)
+}
+
+func (g *Golaravel) Error500(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusInternalServerError)
+}
+
+func (g *Golaravel) Error401(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+func (g *Golaravel) Error403(w http.ResponseWriter, r *http.Request) {
+	g.ErrorStatus(w, http.StatusForbidden)
+}
+
+func (g *Golaravel) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
 }
