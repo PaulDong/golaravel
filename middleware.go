727acc335d200987c4ea0283c967377837466c5d
paul Tung <paul@calgah.com>
2023-04-10 13:35:46 -0400

    add csrf token function

--- middleware.go
+++ middleware.go
@@ -1,8 +1,26 @@
 package golaravel
 
-import "net/http"
+import (
+  "net/http"
+  "strconv"
+  "github.com/justinas/nosurf"
+)
 
 func (g *Golaravel) SessionLoad(next http.Handler) http.Handler {
-	g.InfoLog.Println("SessionLoad called")
-	return g.Session.LoadAndSave(next)
+  g.InfoLog.Println("SessionLoad called")
+  return g.Session.LoadAndSave(next)
+}
+
+func (g *Golaravel) NoSurf(next http.Handler) http.Handler {
+  csrfHandler := nosurf.New(next)
+  secure, _ := strconv.ParseBool(g.config.cookie.secure)
+
+  csrfHandler.SetBaseCookie(http.Cookie{
+    HttpOnly: true,
+    Path: "/",
+    Secure: secure,
+    SameSite: http.SameSiteStrictMode,
+    Domain: g.config.cookie.domain,
+  })
+  return csrfHandler
 }
